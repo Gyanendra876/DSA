5c4b7e9d007c99581d8cce18c4b4c4413bd39940
Gyanendra876 <harshsinha06z@gmail.com>
2026-09-03 02:00:29 +0530

    Time: 17 ms (92.43%) | Memory: 45.9 MB (31.82%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -5,21 +5,24 @@ class Solution {
        Arrays.sort(nums);
        List<List<Integer>> ls=new ArrayList<>();
        for(int i=0; i<nums.length; i++){
+        long temp;
         if(i>0 && nums[i]==nums[i-1])continue;
-        for(int j=i+1; j<nums.length; j++){
-            if(j>i+1 && nums[j]==nums[j-1])continue;
+        for(int j=i+1; j<nums.length-2; j++){
+            temp=(long)nums[i]+nums[j];
+            long ans=(long)target-temp;
+            if(j>i+1 && nums[j]==nums[j-1])continue;   
             k=j+1;
             z=nums.length-1;
             while(k<z){
-                long result=(long)nums[i]+nums[j]+nums[k]+nums[z];
-                if(result==target){
+                long result=(long)nums[k]+nums[z];
+                if(result==ans){
                     ls.add(Arrays.asList(nums[i],nums[j],nums[k],nums[z]));
                     while(k<z && nums[k]==nums[k+1])k++;
                     while(k<z && nums[z]==nums[z-1])z--;
                     k++;
                     z--;
                 }
-                else if(result>target){
+                else if(result>ans){
                     z--;
 
                 }
